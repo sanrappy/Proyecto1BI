cd54a877f7256fe6d058ea1dad20b6a9a63d444b
Juan Andres Jaramillo Penagos <69519712+juanAndresJaramilloP@users.noreply.github.com>
2024-04-18 15:33:11 -0500

    listo back

--- back/main.py
+++ back/main.py
@@ -27,6 +27,7 @@ snow_stemmer = SnowballStemmer(language="spanish")
 import dill as pickle
 #from pipeline_functions import df_to_array, preprocess
 
+# ----------------------------- DEFINICION API -----------------------------
 
 app = FastAPI()
 
@@ -40,87 +41,31 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
-# ----------------------------- CARGA DE DATOS Y MODELO -----------------------------
-
-df = pd.read_csv("../data/tipo2_entrenamiento_estudiantes.csv", sep=',', encoding = 'utf-8')
-classes = sorted(df['Class'].unique())  # Clases: 1, 2, 3, 4 y 5.
-with open('logistical_optimal_pipeline.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-# ----------------------------- Validacion del funcionamiento del modelo: -----------------------------
-# revs = df.copy()
-# X_train, X_test, y_train, y_test = train_test_split(revs['Review'], revs['Class'], test_size=0.2, random_state=1)
-
-# y_test_pred = model.predict(X_test)
-
-
-# print("Metricas para el conjunto de test")
-# print('Exactitud: %.2f' % accuracy_score(y_test, y_test_pred))
-# print("Recall: {:.3f}".format(recall_score(y_test, y_test_pred, average='weighted')))
-# print("Precisión: {:.3f}".format(precision_score(y_test, y_test_pred, average='weighted')))
-# print("Puntuación F1: {:.3f}".format(f1_score(y_test, y_test_pred, average='weighted')))
-# print()
-
-
-# # Impresión de los tokens más significativos por clase después de ajustar el modelo
-
-# # Se accede al TfidfVectorizer y al objeto LogisticRegression del pipeline
-# vectorizer_logistic = model.named_steps['vect']
-# clf_logistic = model.named_steps['clf']
-
-# # Coeficientes (pesos) del modelo de regresión logística
-# coefficients = clf_logistic.coef_
-
-# # Nombre de las características (tokens)
-# feature_names_logistic = vectorizer_logistic.get_feature_names_out()
-
-# # Estructura para almacenar los tokens más significativos de cada clase.
-# significant_tokens_logistic = {}
-
-# top_n = 10
-# classes = sorted(revs['Class'].unique())  # Clases: 1, 2, 3, 4 y 5.
-
-# for i, class_name in enumerate(classes):
-#     class_coefficients = coefficients[i]
-#     top_indices = class_coefficients.argsort()[-top_n:][::-1]
-#     significant_tokens_logistic[class_name] = [feature_names_logistic[idx] for idx in top_indices]
-
-# for class_name, tokens in significant_tokens_logistic.items():
-#     print(f"Class: {class_name}")
-#     print("Top significant tokens:", tokens)
-#     print()
-
-# ----------------------------- API ENDPOINTS -----------------------------
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
+## Definicion de clases para los endpoints
 class Review(BaseModel):
     review: str
     
 class DataFile(BaseModel):
     file_path: str
 
+# ----------------------------- CARGA DE DATOS Y MODELO -----------------------------
+
+df = pd.read_csv("../data/tipo2_entrenamiento_estudiantes.csv", sep=',', encoding = 'utf-8')
+
+X_train, X_test, y_train, y_test = train_test_split(df['Review'], df['Class'], test_size=0.2, random_state=1)
+
+classes = sorted(df['Class'].unique())  # Clases: 1, 2, 3, 4 y 5.
+with open('logistical_optimal_pipeline.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# ----------------------------- API ENDPOINTS -----------------------------
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None, m: Union[str, None] = None):
-    return {"item_id": item_id,
-            "q": q,
-            "m": m
-           }
-    
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
 @app.post("/retrain_model_with_new_datafile")
-async def retrain_model_with_new_datafile(file_path: Union[str, None] = None):
+async def retrain_model_with_new_datafile(file_path: DataFile = None):
 
     if file_path is None:
         raise HTTPException(status_code=404, detail="No file path provided")
@@ -129,9 +74,12 @@ async def retrain_model_with_new_datafile(file_path: Union[str, None] = None):
     global df
     global model
     global classes
+    global X_train, X_test, y_train, y_test
+    
     try:
-        file_path = f"../data/{file_path}"
-        df = pd.read_csv(file_path, sep=',')
+        file_path_relative = f"../data/{file_path.file_path}"
+        print(file_path_relative)
+        df = pd.read_csv(file_path_relative, sep=',')
         
         # Verifica si el dataframe tiene exactamente dos columnas
         if len(df.columns) != 2:
@@ -152,7 +100,8 @@ async def retrain_model_with_new_datafile(file_path: Union[str, None] = None):
         # Si ocurre algun otro error
         raise HTTPException(status_code=500, detail=str(e))
     
-    model.fit(df['Review'], df['Class'])
+    X_train, X_test, y_train, y_test = train_test_split(df['Review'], df['Class'], test_size=0.2, random_state=1)
+    model.fit(X_train, y_train)
     with open('logistical_optimal_pipeline.pkl', 'wb') as f:
         pickle.dump(model, f, recurse=True)
 
@@ -163,8 +112,7 @@ async def test_model():
     global df 
     global model
     global classes
-    
-    X_train, X_test, y_train, y_test = train_test_split(df['Review'], df['Class'], test_size=0.3, random_state=1)
+    global X_test, y_test
 
     y_test_pred = model.predict(X_test)
     
